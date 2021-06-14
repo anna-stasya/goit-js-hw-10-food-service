38ab6196650728d33a20c1556cd37983a3739a99
Anastasiia Sanatar <sanatar.nastya@gmail.com>
2021-06-14 12:57:11 +0300

    смена темы

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,4 +3,32 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const
+const bodyRef = document.querySelector('body');
+const switchRef = document.querySelector('#theme-switch-toggle');
+
+// дефолтная тема
+
+const defaultTheme = localStorage.getItem('Theme');
+if (defaultTheme === 'DARK') {
+  bodyRef.classList.add(Theme.DARK);
+  switchRef.checked = true;
+}
+
+//смена темы со светлой на тёмную
+
+bodyRef.addEventListener('change', onThemeChenge);
+
+function onThemeChenge(event) {
+  if (event.target.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+
+    localStorage.setItem('Theme', Theme.DARK);
+  }
+  else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
+}
